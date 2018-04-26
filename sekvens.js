1d216492edea7ca4f6d618cf06795a31f433735d
rebe4175 <rebe4175@hotmail.com>
2018-04-26 13:30:01 +0200

    fadein n out

--- sekvens.js
+++ sekvens.js
@@ -1,9 +1,49 @@
 var antalKlik
 var erAlleKlikket = false
 
-$(window).on("load", titelBillede);
+$(window).on("load", skaermEt);
 
 
-function titelBillede() {
+function skaermEt() {
+
+    console.log("skaerm et");
+
+    $("#skaerm2").hide();
+    $("#skaerm3").hide();
+    $("#skaerm4").hide();
+
+    setTimeout(skaermTo, 5000);
+
+}
+
+function skaermTo() {
+
+    console.log("skaerm to");
+
+
+
+    $("#skaerm2").fadeIn();
+    $("#skaerm3").hide();
+    $("#skaerm4").hide();
+
+    setTimeout(skaermTre, 5000);
+}
+
+function skaermTre() {
+
+    console.log("skaerm tre");
+
+    $("#skaerm3").fadeIn();
+    $("#skaerm4").hide();
+
+
+    setTimeout(skaermFire, 5000);
+}
+
+function skaermFire() {
+
+    console.log("skaerm fire");
+
+    $("#skaerm4").fadeIn();
 
 }
