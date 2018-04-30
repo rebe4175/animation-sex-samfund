8b303e90fa093813c4c5a03f69f3c1c442fac4e8
rebe4175 <rebe4175@hotmail.com>
2018-04-30 10:05:05 +0200

    kode m. klikbare billeder

--- sekvens.js
+++ sekvens.js
@@ -1,4 +1,4 @@
-var antalKlik
+var billeder
 var erAlleKlikket = false
 
 $(window).on("load", skaermEt);
@@ -11,8 +11,9 @@ function skaermEt() {
     $("#skaerm2").hide();
     $("#skaerm3").hide();
     $("#skaerm4").hide();
+    $("#fuldskaerm").hide();
 
-    setTimeout(skaermTo, 5000);
+    setTimeout(skaermTo, 2000);
 
 }
 
@@ -20,13 +21,12 @@ function skaermTo() {
 
     console.log("skaerm to");
 
-
-
     $("#skaerm2").fadeIn();
     $("#skaerm3").hide();
     $("#skaerm4").hide();
+    $("#fuldskaerm").hide();
 
-    setTimeout(skaermTre, 5000);
+    setTimeout(skaermTre, 2000);
 }
 
 function skaermTre() {
@@ -35,9 +35,9 @@ function skaermTre() {
 
     $("#skaerm3").fadeIn();
     $("#skaerm4").hide();
+    $("#fuldskaerm").hide();
 
-
-    setTimeout(skaermFire, 5000);
+    setTimeout(skaermFire, 1000);
 }
 
 function skaermFire() {
@@ -45,5 +45,80 @@ function skaermFire() {
     console.log("skaerm fire");
 
     $("#skaerm4").fadeIn();
+    $("#fuldskaerm").hide();
+
+
+
+    $("#betina_sprite").addClass("reaktion");
+
+    setTimeout(fuldSkaerm, 7000);
+
+}
+
+function fuldSkaerm() {
+
+    console.log("stor skaerm");
+
+    $("#fuldskaerm").fadeIn();
+
+    klikIgen();
+
+}
+
+
+function klikIgen() {
+    console.log("klik på billede igen");
+    $(".billede").addClass("puls");
+    $(".billede").on("click", klikPaaBillede);
+    setTimeout(tidenergaaet, 5000);
+}
+
+
+function klikPaaBillede() {
+    console.log("klik på billede");
+
+    $(".billede").removeClass("puls");
+    $(".billede").off("click", klikPaaBillede);
+    $(this).fadeOut();
+    billeder++;
+
+    faerdig();
+}
+
+
+function faerdig() {
+
+    console.log("færdig med klik");
+
+
+    if (billeder >= 10) {
+        erAlleKlikket = false;
+
+
+    } else {
+        console.log("klik på billede igen");
+        klikIgen();
+    }
+
+}
+
+function tidenergaaet() {
+
+    console.log("tiden er gaaet");
+
+    if (erAlleKlikket == false) {
+
+        privatSnakB();
+
+    }
+}
+
+
+function privatSnakB() {
+
+    console.log("slutning B");
+    $(".billede").removeClass("puls");
+    $(".billede").off("click", klikPaaBillede);
+    $(".slutning_a").addClass("slutningA");
 
 }
