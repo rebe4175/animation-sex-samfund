51bb93109f1572f63916207fbd46adb2fc32809f
rebe4175 <rebe4175@hotmail.com>
2018-05-03 22:55:38 +0200

    final (probably)

--- sekvens.js
+++ sekvens.js
@@ -10,10 +10,24 @@ function titelBillede() {
 
     $("#skaerm1").hide();
     $("#tekst_1").hide();
+    $("#tekst_2").hide();
     $("#skaerm2").hide();
     $("#skaerm3").hide();
     $("#skaerm4").hide();
+    $("#knap1").hide();
+    $("#knap2").hide();
+    $("#stopdeling").hide();
     $("#fuldskaerm").hide();
+    $("#hjaelpcille").hide();
+    $("#slutningA").hide();
+    $("#slutningB").hide();
+    $("#nummer1").hide();
+    $("#nummer2").hide();
+    $("#nummer3").hide();
+    $("#privatsnak").hide();
+    $("#nogen").hide();
+    $("#nogen2").hide();
+
 
     $("#titelbillede").on("click", skaermEt);
 
@@ -22,77 +36,140 @@ function titelBillede() {
 function skaermEt() {
 
     console.log("skaerm et");
+
+    // $("#baggrundslyd")[0].play();
     $("#skaerm1").fadeIn();
-    $("#skaerm2").hide();
-    $("#skaerm3").hide();
-    $("#skaerm4").hide();
-    $("#fuldskaerm").hide();
     $("#titelbillede").hide();
     $("#tekst_1").fadeIn();
+    $("#baggrundslyd")[0].play();
 
-
-    setTimeout(skaermTo, 11000);
-
+    $("#scene").on("animationend", skaermTo);
 }
 
 function skaermTo() {
 
     console.log("skaerm to");
 
-    $("#skaerm2").fadeIn();
-    $("#skaerm3").hide();
-    $("#skaerm4").hide();
-    $("#fuldskaerm").hide();
-    $("#titelbillede").hide();
-    $("#tekst_1").fadeOut();
+    $("#scene").off("animationend", skaermTo);
 
-    // setTimeout(skaermTre, 2000);
+    $("#skaerm2").fadeIn();
+    $("#tekst_1").fadeOut();
+    $("#knap1").delay(2000).fadeIn();
+    $("#knap2").delay(2000).fadeIn();
+    $("#nogen").show();
+
+    $("#modtag6")[0].play();
+
+    $("#knap1").on("click", skaermTre);
+    $("#knap2").on("click", videresendIkke);
+
 }
+
+function videresendIkke() {
+
+    console.log("videresend ikke");
+
+    $("#knap1").fadeOut();
+    $("#knap2").fadeOut();
+    $("#tekst_2").show();
+
+    $("#nogen").hide();
+
+    $("#tekst2_container").addClass("tekst2_move");
+    $("#tekst_2").addClass("tekst2_sprite");
+
+    $("#scene").on("animationend", privatSnakA);
+}
+
 
 function skaermTre() {
 
     console.log("skaerm tre");
 
     $("#skaerm3").fadeIn();
-    $("#skaerm4").hide();
-    $("#fuldskaerm").hide();
-    $("#titelbillede").hide();
 
-    //  setTimeout(skaermFire, 1000);
+    $("#knap1").hide();
+    $("#knap2").hide();
+    $("#nogen").hide();
+
+    $("#nummer1").fadeIn();
+
+    $("#modtag")[0].play();
+    setTimeout(sms2, 1000);
+
+}
+
+
+function sms2() {
+
+    console.log("sms 2");
+
+    $("#nummer2").fadeIn();
+    $("#modtag2")[0].play();
+    setTimeout(sms3, 1000);
+}
+
+function sms3() {
+
+    console.log("sms 3");
+
+    $("#nummer3").fadeIn();
+    $("#modtag3")[0].play();
+
+    setTimeout(skaermFire, 2000);
 }
 
 function skaermFire() {
 
+    $("#nummer1").hide();
+    $("#nummer2").hide();
+    $("#nummer3").hide();
+    $("#nogen2").fadeIn();
+
     console.log("skaerm fire");
 
     $("#skaerm4").fadeIn();
-    $("#fuldskaerm").hide();
-    $("#titelbillede").hide();
 
     $("#betina_sprite").addClass("reaktion");
 
-    //  setTimeout(fuldSkaerm, 6000);
+    $("#modtag4")[0].play();
 
+    setTimeout(trykStart, 5000);
+}
+
+function trykStart() {
+
+    console.log("tryk på start");
+
+    $("#hjaelpcille").show();
+    $("#stopdeling").show();
+    $("#nogen2").hide();
+
+    $("#stopdeling").on("click", fuldSkaerm);
 }
 
 function fuldSkaerm() {
 
     console.log("stor skaerm");
 
+    $("#hjaelpcille").hide();
+    $("#stopdeling").hide();
     $("#titelbillede").hide();
-
     $("#fuldskaerm").fadeIn();
+
+    $("#baggrundslyd")[0].pause();
+
+    $("#countdown")[0].play();
 
     klikIgen();
 
 }
 
-
 function klikIgen() {
     console.log("klik på billede igen");
     $(".billede").addClass("puls");
     $(".billede").on("click", klikPaaBillede);
-    setTimeout(tidenergaaet, 6200);
+    setTimeout(tidenergaaet, 6000);
 }
 
 
@@ -112,7 +189,6 @@ function faerdig() {
 
     console.log("færdig med klik");
 
-
     if (billeder >= 10) {
         erAlleKlikket = false;
 
@@ -128,19 +204,42 @@ function tidenergaaet() {
 
     console.log("tiden er gaaet");
 
-    if (erAlleKlikket == false) {
 
-        $(".billede").removeClass("puls");
-        $(".billede").off("click", klikPaaBillede);
-    }
+    $(".billede").removeClass("puls");
+    $(".billede").off("click", klikPaaBillede);
+
+    $("#countdown")[0].pause();
+    $("#lose")[0].play();
+
+    setTimeout(privatSnakB, 5000);
+
+}
+
+function privatSnakA() {
+
+    console.log("slutning A");
+
+    $("#scene").off("animationend", privatSnakA);
+    $(".billede").on("click", klikPaaBillede);
+
+    $("#tekst_2").hide();
+    $("#slutningA").show();
+    $("#privatsnak").show();
+
+
 }
 
 function privatSnakB() {
 
-    console.log("slutning B");
+    console.log("slutning b");
+
+    $("#fuldskaerm").hide();
     $(".billede").removeClass("puls");
     $(".billede").off("click", klikPaaBillede);
 
-    $(".slutning_a").addClass("slutningA");
+    $("#slutningB").show();
+    $("#privatsnak").show();
+
+
 
 }
